de84d21b96379f6dc21b7df18dda7a32cb3da814
SIVA-1116 <sp0379028@gmail.com>
2024-12-11 14:46:04 +0530

    [621] [Time Beats: 54.50%] [Memory Beats: 43.71%] - LeetPush

---
+++ 621-Task-Scheduler.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        int[] freq=new int[26];
+        for(char ch:tasks){
+            freq[ch-'A']++;
+        }
+        PriorityQueue<Integer> pq=new PriorityQueue<>(Collections.reverseOrder());
+        for(int i:freq){
+            if(i!=0){
+                pq.add(i);
+            }
+        }
+        int time=0;
+        
+        while(pq.size()!=0){
+            List<Integer> temp=new ArrayList<>();
+            for(int i=0;i<=n;i++){
+                if(!pq.isEmpty()){
+                    temp.add(pq.poll()-1);
+                }
+            }
+            for(int fre:temp){
+                if(fre>0){
+                    pq.add(fre);
+                }
+            }
+            time+=pq.isEmpty() ? temp.size() : n+1;
+        }
+        return time;
+    }
+}
