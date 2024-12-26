91a60d0cd8d876ecd2a1ee4f3676978710a45b86
SIVA-1116 <sp0379028@gmail.com>
2024-12-26 14:30:53 +0530

    [198] [Time Beats: 100.00%] [Memory Beats: 36.31%] - LeetPush

--- 198-House-Robber.java
+++ 198-House-Robber.java
@@ -1,15 +1,17 @@
 class Solution {
     public int rob(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return 0;
-        }
-        int prev1 = 0; 
-        int prev2 = 0;
-        for (int num : nums) {
-            int current = Math.max(prev1, prev2 + num);
-            prev2 = prev1;
-            prev1 = current;
-        }
-        return prev1;
+        int n=nums.length;
+        int[] dp=new int[n];
+        Arrays.fill(dp,-1);
+        return helper(nums,n-1,dp);
+    }
+    public int helper(int[] nums,int n,int[] dp){
+        if(n<0) return 0;
+        if(n==0) return nums[0];
+        if(dp[n]!=-1) return dp[n];
+        int ra=helper(nums,n-1,dp)+0;
+        int la=helper(nums,n-2,dp)+nums[n];
+        dp[n]=Math.max(la,ra);
+        return dp[n];
     }
 }
